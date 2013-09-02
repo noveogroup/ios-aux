28f24f39fd1ed740498cbb14146c216bb44210ae
misterBadger <liniandv@gmail.com>
2013-09-02 23:12:46 +0700

    CopyBytesWithStride function and sample project added

---
+++ ios-aux/acceleration/acceleration.h
@@ -0,0 +1,14 @@
+//
+//  acceleration.h
+//  Example
+//
+//  Created by liniandv on 9/2/13.
+//
+//
+
+#ifndef Example_acceleration_h
+#define Example_acceleration_h
+
+
+
+#endif
---
+++ ios-aux/acceleration/acceleration.m
@@ -0,0 +1,9 @@
+//
+//  acceleration.cpp
+//  Example
+//
+//  Created by liniandv on 9/2/13.
+//
+//
+
+#include "acceleration.h"
