6bf65875a5b1d6a952adfff8ee365d98ac6c1bdc
misterBadger <misterBadger@users.noreply.github.com>
2013-09-30 17:02:34 +0700

    Update acceleration.h

--- ios-aux/acceleration/acceleration.h
+++ ios-aux/acceleration/acceleration.h
@@ -12,5 +12,8 @@
  Copies bytes from src to dst with stride.
  i-th byte of src goes to i*stride -th byte of dst (i = 0 for first byte of src)
  
+ Benefit of using this function on ARM is only in debug mode because in release mode
+ compiler optimizes (by default) corresponding C-code and it works as fast as ASM-code
+ 
  */
 void CopyBytesWithStride(UInt8 *src, UInt8 *dst, UInt32 srcSize, UInt32 stride);
